78abded601ee6c26fa5b2872180426beda0c1a1e
Edson M. Souza <edsonmelo754@gmail.com>
2020-10-15 12:07:30 -0300

    feat: added web

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Routes from './routes';
+
+import './styles/global.css';
+import 'leaflet/dist/leaflet.css'
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+
+import mapMarkerImg from '../images/Local.svg';
+import mapIcon from '../utils/mapIcon';
+
+import api from '../services/api';
+
+import '../styles/pages/orphanages-map.css'
+
+interface IOrfhanages {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+}
+
+function OrphanagesMap() {
+  const [orphanages, setOrphanages] = useState<IOrfhanages[]>([]);
+  console.log(orphanages);
+  useEffect(() => {
+    api.get('orphanages').then(response => {
+      setOrphanages(response.data);
+    });
+  }, []);
+
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+          <h2>Escolha um orfanto no mapa</h2>
+          <p>Muita crianças esperando</p>
+        </header>
+
+        <footer>
+          <strong>Mata Grande</strong>
+          <span>Alagoas</span>
+        </footer>
+      </aside>
+
+      <Map
+        center={[-9.125888, -37.7257984]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+
+        {orphanages.map(orphanage => {
+          return (
+            <Marker
+              icon={mapIcon}
+              position={[orphanage.latitude, orphanage.longitude]}
+              key={orphanage.id}
+            >
+              <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                {orphanage.name}
+                <Link to={`/orphanages/${orphanage.id}`}>
+                  <FiArrowRight size={20} color="#FFF" />
+                </Link>
+              </Popup>
+            </Marker>
+          );
+        })}
+      </Map>
+
+      <Link to="/orphanages/create" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
+  );
+}
+
+export default OrphanagesMap;
